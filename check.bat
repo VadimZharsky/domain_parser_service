230d977cf9096ec3845ec261374b670519ba509b
VadimZharsky <vadimzharsky1990@gmail.com>
2025-10-08 13:38:46 +0300

    add check.bat to pass check on windows

---
+++ check.bat
@@ -0,0 +1,19 @@
+@echo off
+setlocal
+
+set "dir=./domain"
+
+echo !!!RUFF FORMAT: %dir%
+ruff format %dir%
+echo ---------------
+
+echo !!!RUFF SORT: %dir%
+ruff check --select I --fix %dir%
+echo ---------------
+
+echo !!!RUFF FIX: %dir%
+ruff check --fix %dir%
+echo ---------------
+
+echo !!!MYPY: %dir%
+mypy %dir%
